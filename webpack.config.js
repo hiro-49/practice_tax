7dc0d080a73e2f3054648541378ecaddd3540008
hiro-49 <kazuki97101@gmail.com>
2019-10-18 15:09:34 +0900

    webpackを利用したプログラムを作成

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,6 @@ module.exports = {
     // 出力するファイル名
     filename: 'bundle.js',
     // 出力先のパス（絶対パスを指定する必要がある）
-    path: path.join(__dirname, 'public/js')
+    path: path.join(__dirname, 'js')
   }
 };
